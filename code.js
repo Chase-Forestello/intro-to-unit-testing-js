89d600215ba3656a1ae1a60b953af9c6d1028528
Chase Forestello <chase.forestello@gmail.com>
2022-06-13 11:53:47 -0500

    Incomplete(finished TDD tests. Time to refactor) - JavaScript I - Testing User Functions - Exercise

---
+++ code.js
@@ -0,0 +1,15 @@
+// Incomplete(finished TDD tests. Time to refactor) - JavaScript I - Testing User Functions - Exercise
+// helloWorld function
+let helloWorld = function () {
+    return "Hello, World!";
+}
+
+function sayHello(input) {
+    if (input === "Alex") {
+        return "Hello, Alex!";
+    } else if (input === "Pat") {
+        return "Hello, Pat!";
+    } else {
+        return "Hello, Jane!";
+    }
+}
---
+++ tests.js
@@ -0,0 +1,34 @@
+// Incomplete(finished TDD tests. Time to refactor) - JavaScript I - Testing User Functions - Exercise
+// Unit tests for the helloWorld function
+describe('helloWorld', function () {
+    it('should be a defined function', function () {
+        expect(typeof helloWorld).toBe('function');
+    });
+    it('should return a string when called', function () {
+        expect(typeof helloWorld()).toBe("string");
+    });
+    it('should return the string "Hello, World!" when executed', function () {
+        expect(helloWorld()).toBe("Hello, World!");
+    });
+    it("should never return 'undefined' when called", function () {
+        expect(helloWorld()).not.toBe(undefined);
+    });
+});
+describe("sayHello", function () {
+    it("should be a defined function", function () {
+        expect(typeof sayHello).toBe("function");
+    });
+    it("should be a string", function () {
+        expect(typeof sayHello()).toBe("string");
+    });
+    it("should return the string 'Hello, Jane!' when executed", function () {
+        expect(sayHello()).toBe("Hello, Jane!");
+    });
+    it("should return the string 'Hello, Alex!' when executed", function () {
+        expect(sayHello("Alex")).toBe("Hello, Alex!");
+    });
+    it("should return the string 'Hello, Pat!' when executed", function () {
+        expect(sayHello("Pat")).toBe("Hello, Pat!");
+    });
+
+});
